626acbd8820f3f946860cb190b46f4a3ec8f48e6
Swabian-Janne <janne.jasmin.jp@gmail.com>
2024-08-16 19:18:14 +0200

    added api to js and id to html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 /** @format */
 
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let weatherCityElement = document.querySelector("#weather-city");
+
+  weatherCityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ef3aab353f2ado9faa8tccd552b00647";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = searchInput.value;
+  let weatherCityElement = document.querySelector("#weather-city");
+  weatherCityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
