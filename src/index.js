5ec45f47aaf428f7fab30656ffdd70bdc6adacd7
Swabian-Janne <janne.jasmin.jp@gmail.com>
2024-08-18 00:05:12 +0200

    added forecast js

--- src/index.js
+++ src/index.js
@@ -54,6 +54,53 @@ function handleSearchSubmit(event) {
   //cityElement.innerHTML = searchInput.value;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "ef3aab353f2ado9faa8tccd552b00647";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+         <div class="weather-forecast-day">
+                    <div class="weather-forecast-date">${formatDay(
+                      day.time
+                    )}</div>
+                    <img src "${
+                      day.condition.icon_url
+                    }" class="weather-forecast-icon" />
+                    <div class="weather-forecast-temperatures">
+                        <div class="weather-forecast-temperature">
+                        <strong>${Math.round(
+                          day.temperature.maximum
+                        )}°</strong></div>
+                        <div class="weather-forecast-temperature">${Math.round(
+                          day.temperature.minimum
+                        )}°</div>
+                    </div>
+                </div>
+        `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
 searchCity("London");
